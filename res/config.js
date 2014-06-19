795cb190ed14fab7224d6e0811f0b8e492b71f87
Mark Deaton <medeaton@hotmail.com>
2014-06-18 19:17:44 -0700

    initial commit

---
+++ res/config.js
@@ -0,0 +1,9 @@
+var config = {
+    "services": {
+        "basemap":"http://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer",
+        "ThreeDosePct_admin1":"http://drumlin:6080/arcgis/rest/services/GPEI/ThreePlusDosePercent/MapServer",
+        "ZeroDosePct_poly":"http://drumlin:6080/arcgis/rest/services/GPEI/ZeroDosePercent/MapServer",
+        "ThreePlusDosePct_poly":"http://drumlin:6080/arcgis/rest/services/GPEI/ThreePlusDosePercent/MapServer"
+    },
+    "initialExtent": {xmin:-9.92173025843839, ymin:-67.3299802178466, xmax:131.819837912305, ymax:66.6470122528879}
+};
