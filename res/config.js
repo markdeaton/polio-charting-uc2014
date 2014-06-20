0f5693d92fd1ef9d6eee417d72144a52862cc895
Mark Deaton <medeaton@hotmail.com>
2014-06-20 16:47:24 -0700

    Updated to work with a different service.

--- res/config.js
+++ res/config.js
@@ -3,7 +3,8 @@ var config = {
         "basemap":"http://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer",
         "ThreeDosePct_admin1":"http://drumlin:6080/arcgis/rest/services/GPEI/ThreePlusDosePercent/MapServer",
         "ZeroDosePct_poly":"http://drumlin:6080/arcgis/rest/services/GPEI/ZeroDosePercent/MapServer",
-        "ThreePlusDosePct_poly":"http://drumlin:6080/arcgis/rest/services/GPEI/ThreePlusDosePercent/MapServer"
+        "ThreePlusDosePct_poly":"http://drumlin:6080/arcgis/rest/services/GPEI/ThreePlusDosePercent/MapServer",
+        "NPAFPRate_poly":"http://gpeiproto/arcgis/rest/services/GPEI/NPAFP_Rate_R6M1/MapServer"
     },
     "initialExtent": {xmin:-1366064.68456421, ymin:-6500946.0502866, xmax:13413589.8747449, ymax:7469081.88976928, "spatialReference":{"wkid":102100}}
 };
