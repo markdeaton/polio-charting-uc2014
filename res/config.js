6b15a9ca431b34bd62ddc54271be81636992b764
Mark Deaton <medeaton@hotmail.com>
2014-06-23 16:56:23 -0700

    Ability to identify/chart more than one layer at a time. (or easily switch id target in the future)

--- res/config.js
+++ res/config.js
@@ -4,7 +4,8 @@ var config = {
         "ThreeDosePct_admin1":"http://drumlin:6080/arcgis/rest/services/GPEI/ThreePlusDosePercent/MapServer",
         "ZeroDosePct_poly":"http://drumlin:6080/arcgis/rest/services/GPEI/ZeroDosePercent/MapServer",
         "ThreePlusDosePct_poly":"http://drumlin:6080/arcgis/rest/services/GPEI/ThreePlusDosePercent/MapServer",
-        "NPAFPRate_poly":"http://gpeiproto/arcgis/rest/services/GPEI/NPAFP_Rate_R6M1/MapServer"
+        "NPAFPRate_poly":"http://gpeiproto/arcgis/rest/services/GPEI/NPAFP_Rate_R6M1/MapServer",
+        "SIA_Campaigns_Tot":"http://gpeiproto.esri.com/arcgis/rest/services/GPEI/SIA_Campaigns_Total/MapServer"
     },
     "initialExtent": {xmin:-1366064.68456421, ymin:-6500946.0502866, xmax:13413589.8747449, ymax:7469081.88976928, "spatialReference":{"wkid":102100}}
 };
