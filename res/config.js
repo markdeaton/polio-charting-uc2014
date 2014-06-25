3872a3e3af015968c7d7ab0d2836c3057e39f79d
Mark Deaton <medeaton@hotmail.com>
2014-06-25 10:06:11 -0700

    Layout fixes

--- res/config.js
+++ res/config.js
@@ -4,6 +4,7 @@ var config = {
         "ZeroDosePct_poly":"http://gpeiproto/arcgis/rest/services/GPEI/ZeroDose_Percent_R6M/MapServer",
         "ThreePlusDosePct_poly":"http://gpeiproto/arcgis/rest/services/GPEI/ThreePlus_Dose_Percent_R6M/MapServer",
         "NPAFPRate_poly":"http://gpeiproto/arcgis/rest/services/GPEI/NPAFP_Rate_R6M1/MapServer",
+        "StoolAdequacyRate_poly":"http://gpeiproto/arcgis/rest/services/GPEI/StoolAdequacyRate_R6M/MapServer",
         "SIA_Campaigns_Tot":"http://gpeiproto.esri.com/arcgis/rest/services/GPEI/SIA_Campaigns_Total/MapServer"
     },
     "initialExtent": {xmin:-1366064.68456421, ymin:-6500946.0502866, xmax:13413589.8747449, ymax:7469081.88976928, "spatialReference":{"wkid":102100}}
